5a0e7b462078cd4581718b9639115666ae1bf645
Vikki-kanyi <kanyivictoria067@gmail.com>
2025-11-13 14:56:56 -0500

    variables

---
+++ variables.js
@@ -0,0 +1,11 @@
+let age = 20;
+console.log(age); 
+
+age = 21;
+console.log(age);
+
+const Name = "Victoria";
+console.log(Name);
+
+//name = "Vikki"; This is an error as you can't change a constant!!
+
