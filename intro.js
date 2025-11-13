1ead113dc344f93850c8b96972c6cab91564f6d0
Vikki-kanyi <kanyivictoria067@gmail.com>
2025-11-13 13:16:57 -0500

    first commit

---
+++ intro.js
@@ -0,0 +1,6 @@
+console.log("Hello World!!");
+console.log("Am glad to learn Js");
+
+let myPet = "cat";
+let petAge = 3;
+console.log("My pet is a " + myPet + " and he is " + petAge + " years old!!")
